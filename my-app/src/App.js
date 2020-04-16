1b0beac879e623f6137b720b5ac63bb551ea300a
Jason Cruz <badmonkey.dev@gmail.com>
2020-04-16 03:36:10 -0500

    setting up project

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <h3>This page is left intentionally blank . . .</h3>
+    </div>
+  );
+}
+
+export default App;
